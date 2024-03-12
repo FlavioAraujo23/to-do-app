a4e3230479109888f1b6dd4ff12ed39dd1bbd9ff
FlavioAraujo23 <flaviolopes1027@gmail.com>
2024-03-12 19:18:36 -0300

    commit initial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './src/screens/Home';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Login } from './src/screens/Login';
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="To-do" component={Home} />
+        <Tab.Screen name="User" component={Login} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/theme/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  white: '#fff',
+  green: '#5ac7aa',
+  gray: '#9ca3af',
+  stone: '#e7e5e499',
+  yellow: '#fef08a99',
+  greenl: '#bbf7d099',
+  greenh: '#34d399',
+  grayl: '#f3f4f6cc'
+}
