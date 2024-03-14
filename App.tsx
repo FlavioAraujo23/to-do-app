87d5a68634954dfcf4841f196e30e9dbf79bc883
FlavioAraujo23 <flaviolopes1027@gmail.com>
2024-03-13 21:20:25 -0300

    feat: add tailwindcss

--- App.tsx
+++ App.tsx
@@ -1,16 +1,14 @@
-import { StatusBar} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import Routes from './src/routes';
-import { theme } from './src/theme';
-
+import { StatusBar } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import Routes from './src/routes'
+import { theme } from './src/theme'
+import './global.css'
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar backgroundColor={theme.colors.green} barStyle={'default'}/>
+      <StatusBar backgroundColor={theme.colors.green} barStyle={'default'} />
       <Routes />
     </NavigationContainer>
-  );
+  )
 }
-
-
